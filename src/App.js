5deba40a52ba032e0b3d3f5e13b490f1ca7cca21
farwaali <farwaali.me@gmail.com>
2019-01-02 02:21:00 +0500

    404 notfound

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import {
-  BrowserRouter,
-  Route,
-  Link,
-  Redirect,
-  withRouter
+    BrowserRouter,
+    Route,
+    Link,
+    Redirect,
+    withRouter,
+    Switch
 } from "react-router-dom";
 import 'admin-lte/bower_components/bootstrap/dist/css/bootstrap.min.css'
 import 'admin-lte/dist/css/AdminLTE.min.css'
@@ -13,51 +14,22 @@ import 'admin-lte/bower_components/Ionicons/css/ionicons.css'
 
 import Login from "./login/Login";
 import Dashboard from "./dashboard/Dashboard";
+import NotFound from "./notfound/404";
 
 class App extends Component {
   render() {
     return (
         <BrowserRouter>
             <div>
-                <Route exact path="/login" component={Login} />
-                <Route exact path={"/(home|profile|)$"} component={Dashboard} />
+                <Switch>
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/(home|profile|)" component={Dashboard} />
+                    <Route exact component={NotFound}/>
+                </Switch>
             </div>
         </BrowserRouter>
     );
   }
 }
-
-const fakeAuth = {
-  isAuthenticated: false,
-  authenticate(cb) {
-    this.isAuthenticated = true;
-    setTimeout(cb, 100); // fake async
-  },
-  signout(cb) {
-    this.isAuthenticated = false;
-    setTimeout(cb, 100);
-  }
-};
-
-function PrivateRoute({ component: Component, ...rest }) {
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        fakeAuth.isAuthenticated ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/login",
-              state: { from: props.location }
-            }}
-          />
-        )
-      }
-    />
-  );
-}
-
 
 export default App;
--- src/login/Login.js
+++ src/login/Login.js
@@ -9,7 +9,7 @@ class Login extends Component {
             <div className="hold-transition login-page">
             <div className="login-box">
                 <div className="login-logo">
-                    <a href="../../index2.html"><b>Admin</b>LTE</a>
+                    <a href="../../index2.html"><b>Smart O Meter</b></a>
                 </div>
                 <div className="login-box-body">
                     <p className="login-box-msg">Sign in to start your session</p>
@@ -36,17 +36,6 @@ class Login extends Component {
                             </div>
                         </div>
                     </form>
-
-                    <div className="social-auth-links text-center">
-                        <p>- OR -</p>
-                        <a href="#" className="btn btn-block btn-social btn-facebook btn-flat">
-                            <i className="fa fa-facebook"></i> Sign in using
-                            Facebook</a>
-                        <a href="#" className="btn btn-block btn-social btn-google btn-flat">
-                            <i className="fa fa-google-plus"></i> Sign in using
-                            Google+</a>
-                    </div>
-
                     <a href="#">I forgot my password</a><br/>
                     <a href="register.html" className="text-center">Register a new membership</a>
                 </div>
